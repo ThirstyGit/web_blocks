b60cdba00377e9c1deb26379e9904dcb294efa91
James Zaman <jzaman182040@bscse.uiu.ac.bd>
2021-11-29 20:54:46 +0600

    It also works with compressed .rar, .zip, .7zip formats.

--- Components/upload/upload.js
+++ Components/upload/upload.js
@@ -4,6 +4,13 @@ const mainButton = document.querySelector("#main-button");
 const previewImage = document.querySelector("#preview-image");
 const uploadContainer = document.querySelector("#upload-container");
 const cancelButton = document.querySelector("#cancel-button");
+const uploadText = document.querySelector("#upload-text");
+
+// Links for images. This will be used when
+// anything other than images are uploaded.
+const images = {
+  rar: "https://img.icons8.com/fluency/48/000000/winrar.png",
+};
 
 // code to make the beautiful upload work.
 mainButton.addEventListener("click", () => {
@@ -14,15 +21,33 @@ cancelButton.addEventListener("click", () => {
   fileInput.value = "";
   previewImage.style.display = "none";
   previewImage.src = "";
+  // This classes are to properly show image
+  // previews that we want to upload.
   uploadContainer.classList.remove("active");
+  uploadText.classList.remove("hidden");
 });
 
 fileInput.addEventListener("change", function () {
   if (this.files[0]) {
     const reader = new FileReader();
     reader.onload = () => {
-      const result = reader.result;
-      previewImage.src = result;
+      console.log(this.files);
+      if(this.files[0].type.includes('image')) {
+        const result = reader.result;
+        previewImage.src = result;
+        // This classes are to properly show image
+        // previews that we want to upload.
+        previewImage.classList.add("preview-image");
+        uploadText.classList.remove('hidden');
+      }
+      else {
+        previewImage.src = images.rar;
+        // Without this class we show the image as is.
+        previewImage.classList.remove("preview-image");
+        // Hiding the unnecessary text.
+        // Might show useful text in the future.
+        uploadText.classList.add("hidden");
+      }
       uploadContainer.classList.add("active");
     };
     reader.readAsDataURL(this.files[0]);
